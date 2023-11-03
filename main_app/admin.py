8ad973baa52fa9eae7c475a0bd5b0e92a8199a65
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-11-03 11:13:13 +0300

    init

--- main_app/admin.py
+++ main_app/admin.py
@@ -11,6 +11,12 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'patronymic', 'organization')
     search_fields = ('email', 'first_name', 'last_name', 'patronymic', 'organization__name')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'date_joined', 'organization')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'patronymic', 'organization')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
 
 class LifeSituationAdmin(admin.ModelAdmin):
